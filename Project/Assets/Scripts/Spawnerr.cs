87c408f9c60dd672c5a8b19eb590ffec9edc9eae
Mohamad <79073457+Salman-001@users.noreply.github.com>
2022-03-16 17:05:33 +0200

    commit 10

---
+++ Project/Assets/Scripts/Spawnerr.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerr : MonoBehaviour
+{
+    public GameObject[] spawners;
+
+    public GameObject enemy;
+
+    private void Start(){
+
+        spawners = new GameObject[5];
+
+        for(int i = 0; i< spawners.Length; i++){
+
+            spawners[i] = transform.GetChild(i).gameObject;
+
+        }
+
+    }
+
+    private void Update(){
+
+        if(Input.GetKeyDown(KeyCode.T)){
+            SpawenEnemy();
+        }
+
+    }
+
+    private void SpawenEnemy(){
+
+        int spawnerId = Random.Range(0, spawners.Length);
+        Instantiate(enemy, spawners[spawnerId].transform.position, spawners[spawnerId].transform.rotation);
+
+    }
+}
